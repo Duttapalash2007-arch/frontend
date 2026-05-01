3e7ed07e5091ba8322d5429c1c0323fa3ea6073f
Palash Dutta <duttapalash2007@gmail.com>
2026-05-02 00:54:13 +0530

    Connect frontend to deployed backend

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://backend-chi-red-56.vercel.app',
         changeOrigin: true
       }
     }
